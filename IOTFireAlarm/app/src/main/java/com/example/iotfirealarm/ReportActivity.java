adcaf62467cfd13aca3ab39597096171dcdaeaf7
BanYerin <xlvl98@gmail.com>
2019-12-30 23:57:42 +0900

    화재 신고 기능 데이터베이스 연동
    SQLite를 이용하여 사용자 정보 데이터베이스 연동

--- IOTFireAlarm/app/src/main/java/com/example/iotfirealarm/ReportActivity.java
+++ IOTFireAlarm/app/src/main/java/com/example/iotfirealarm/ReportActivity.java
@@ -1,8 +1,8 @@
 /*
 -작성자: 2017038023 반예린
 -해당 소스파일 정보: 화재 신고하기 화면 및 화재 신고에 대한 기능.
-                    신고 버튼을 클릭하면 시스템에 저장된 신고 연락처와 사용자 위치정보를 이용하여 화재 신고 메시지를 자동으로 전송.
--구현 완료된 기능: 관리자에게 신고, 소방서로 신고, 메시지 전송, 홈 화면으로 이동에 대한 기능.
+                    신고 버튼을 클릭하면 DB에 저장된 신고 연락처와 사용자 위치정보를 이용하여 화재 신고 메시지를 자동으로 전송.
+-구현 완료된 기능: 관리자에게 신고, 소방서로 신고, 메시지 전송, 홈 화면으로 이동, DB 연동에 대한 기능.
 -테스트 환경: SAMSUNG Galaxy S7(AVD), API 22
  */
 
@@ -10,18 +10,43 @@ package com.example.iotfirealarm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.net.Uri;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.view.View;
 
 public class ReportActivity extends AppCompatActivity {
+    UserInfoDBHelper mHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+
+        mHelper=new UserInfoDBHelper(this);
+    }
+
+    //DB관리를 위한 도우미 클래스
+    class UserInfoDBHelper extends SQLiteOpenHelper{
+        public UserInfoDBHelper(Context context){
+            super(context, "UserInfo.db", null, 1);
+        }
+
+        public void onCreate(SQLiteDatabase db){
+            db.execSQL("create table UserInfo(infoNum integer primary key autoincrement, addr text, tel text);");
+            //db.execSQL("insert into UserInfo values(null, '충북 청주시 서원구 충대로1 충북대학교', '010-1111-1111');"); //테스트용으로 삽입한 레코드
+        }
+
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+            db.execSQL("drop table if exists UserInfo");
+            onCreate(db);
+        }
     }
 
     //관리자 신고 메소드
@@ -29,6 +54,23 @@ public class ReportActivity extends AppCompatActivity {
         String tel="010-7455-3507"; //메시지를 전송할 임시 관리자 번호. 나중에 DB에서 받아와야함
         String addr="충북 청주시 서원구 개신동 A번지 101호"; //임시 사용자 주소. 나중에 DB에서 받아와야함
         String msg="화재 발생!! 도와주세요!\n 화재 발생지:"+addr; //화재 신고 메시지 내용
+
+        //DB관련 변수 선언
+        SQLiteDatabase db;
+        ContentValues row;
+        Cursor cursor;
+
+        db=mHelper.getReadableDatabase();
+        cursor=db.rawQuery("select addr, tel from UserInfo", null);
+
+        //결과가 존재하는 동안 레코드를 읽어와 각 필드에 대입
+        while(cursor.moveToNext()){
+            addr=cursor.getString(0);
+            tel=cursor.getString(1);
+            msg="화재 발생!! 도와주세요!\n 화재 발생지:"+addr; //화재 신고 메시지 내용
+        }
+
+        mHelper.close();
 
         sendSMS(tel, msg); //화재 신고 메시지 전송
 
@@ -40,7 +82,24 @@ public class ReportActivity extends AppCompatActivity {
         String addr="충북 청주시 서원구 개신동 A번지 101호"; //임시 사용자 주소. 나중에 DB에서 받아와야함
         String msg="화재 발생!! 도와주세요!\n 화재 발생지:"+addr; //화재 신고 메시지 내용
 
+        //DB관련 변수 선언
+        SQLiteDatabase db;
+        ContentValues row;
+        Cursor cursor;
+
+        db=mHelper.getReadableDatabase();
+        cursor=db.rawQuery("select addr, tel from UserInfo", null);
+
+        //결과가 존재하는 동안 레코드를 읽어와 각 필드에 대입
+        while(cursor.moveToNext()){
+            addr=cursor.getString(0);
+            msg="화재 발생!! 도와주세요!\n 화재 발생지:"+addr; //화재 신고 메시지 내용
+        }
+
+        mHelper.close();
+
         sendSMS(tel, msg); //화재 신고 메시지 전송
+
     }
 
     //메시지 전송 메소드
