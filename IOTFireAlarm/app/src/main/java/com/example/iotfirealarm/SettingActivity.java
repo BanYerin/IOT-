50a79ff4f9f57ce0fecd3b182647609f4ed9014b
BanYerin <xlvl98@gmail.com>
2020-01-04 19:58:12 +0900

    사용자 정보 설정과 관련된 메소드 구현, 데이터베이스 연동
    -사용자 정보 등록 및 수정, 등록되어 있는 기존 사용자 정보 출력 기능 메소드 구현
    -SQLite를 이용하여 사용자 정보 데이터베이스 연동

--- IOTFireAlarm/app/src/main/java/com/example/iotfirealarm/SettingActivity.java
+++ IOTFireAlarm/app/src/main/java/com/example/iotfirealarm/SettingActivity.java
@@ -1,6 +1,16 @@
+/*
+-작성자: 2015023025 배나영, 2017038023 반예린
+-해당 소스파일 정보: 사용자 정보 등록 및 수정에 대한 기능.
+                    사용자 주소와 신고 연락처를 입력한 뒤 등록 버튼을 클릭하면 DB에 존재하는 기존 정보를 삭제하고 새로 입력한 정보를 삽입함.
+-구현 완료된 기능: 사용자 정보 등록 및 수정, 등록되어 있는 기존 사용자 정보 출력, DB 연동에 대한 기능.
+-테스트 환경: SAMSUNG Galaxy S7(AVD), API 22
+ */
+
 package com.example.iotfirealarm;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
@@ -14,6 +24,7 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class SettingActivity extends AppCompatActivity {
@@ -28,11 +39,65 @@ public class SettingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_setting);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-    }
-
-    public void registerInfo(View v){
 
         mHelper=new UserInfoDBHelper(this);
+        printUserInfo(); //기존에 등록되어 있는 사용자 정보를 화면에 출력
+
+    }
+
+    //DB관리를 위한 도우미 클래스
+    class UserInfoDBHelper extends SQLiteOpenHelper{
+        public UserInfoDBHelper(Context context){
+            super(context, "UserInfo.db", null, 1);
+        }
+
+        public void onCreate(SQLiteDatabase db){
+            db.execSQL("create table UserInfo(infoNum integer primary key autoincrement, addr text, tel text);");
+        }
+
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+            db.execSQL("drop table if exists UserInfo");
+            onCreate(db);
+        }
+    }
+
+    //사용자 정보 출력 메소드
+    public void printUserInfo(){
+        //사용자 주소 정보와 관리자 전화번호를 EditText형으로 받아옴
+        EditText userAddrEditText=findViewById(R.id.userAddress);
+        EditText adminNumEditText=findViewById(R.id.adminPhoneNumber);
+
+        //사용자 주소 정보와 관리자 전화번호를 TextView형으로 받아옴
+        TextView existUserInfo=(TextView)findViewById(R.id.existUserInfo);
+
+        //DB관련 변수 선언
+        SQLiteDatabase db;
+        ContentValues row;
+        Cursor cursor;
+
+        db=mHelper.getReadableDatabase();
+        cursor=db.rawQuery("select addr, tel from UserInfo", null);
+
+        //결과가 존재하는 동안 레코드를 읽어와 각 필드에 대입
+        while(cursor.moveToNext()){
+            userAddrString=cursor.getString(0);
+            adminPhoneNumString=cursor.getString(1);
+        }
+
+        //현재 DB에 존재하는 사용자 주소와 관리자 연락처를 각 에디트텍스트와 텍스트뷰에 출력
+        userAddrEditText.setText(userAddrString);
+        adminNumEditText.setText(adminPhoneNumString);
+        existUserInfo.setText("<현재 등록되어 있는 정보>"+"\n-사용자 주소: "+userAddrString+"\n-신고 연락처: "+adminPhoneNumString);
+
+    }
+
+    //사용자 정보 등록 및 수정 메소드
+    public void OnclickRegister(View v){
+        //DB관련 변수 선언
+        SQLiteDatabase db;
+        ContentValues row;
+        db=mHelper.getWritableDatabase();
+
 
         // 사용자 주소 정보와 관리자 전화번호를 EditText형으로 받아옴
         EditText userAddrEditText=findViewById(R.id.userAddress);
@@ -42,32 +107,19 @@ public class SettingActivity extends AppCompatActivity {
         userAddrString=userAddrEditText.getText().toString();
         adminPhoneNumString=adminNumEditText.getText().toString();
 
+
+        //UserInfo 테이블에 존재하는 기존의 사용자 정보를 삭제하고 새로 입력받은 정보를 삽입함으로써 사용자 정보를 업데이트함
+        //UserInfo 테이블에 존재하는 모든 레코드 삭제
+        db.execSQL(("delete from UserInfo;"));
+        //사용자로 부터 입력받은 사용자 주소와 관리자 연락처를 UserInfo 테이블에 삽입
+        db.execSQL("insert into UserInfo values(null, '"+userAddrString+"', '"+adminPhoneNumString+"');");
+
+        printUserInfo(); //변경된 사용자 정보를 화면에 출력
+
         // 받아온 사용자 주소와 관리자 전화번호 정보를 화면에 띄워줌
-        Toast.makeText(getApplicationContext(),"사용자주소 : "+userAddrString+"/ 관리자 전화번호 : "+adminPhoneNumString,Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(),"사용자주소 : "+userAddrString+"\n관리자 연락처 : "+adminPhoneNumString+"\n등록 완료",Toast.LENGTH_LONG).show();
 
-    }
-
-    // DB 관리를 위한 도우미 클래스
-    class UserInfoDBHelper extends SQLiteOpenHelper{
-
-        public UserInfoDBHelper(@Nullable Context context) {
-            super(context, "UserInfoDB",null, 1);
-        }
-
-        @Override
-        public void onCreate(SQLiteDatabase db) {
-            db.execSQL("create table UserInfo(infoNum integer primary key autoincrement, addr text, tel text);");
-        }
-
-        // 사용자 주소와 관리인 전화번호 정보를 어떻게 넘겨줄 것인지 - 매개변수 수정하니 오류 뜸
-        @Override
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-            // db.execSQL("drop table if exists UserInfo");
-            // 주소와 관리자 정보 업데이트하기
-            // 있으면 update로 데이터 값을 바꿔주고 없으면 insert로 삽입
-
-            db.execSQL("insert into UserInfo values ("+userAddrString+","+adminPhoneNumString+" )"); // 사용자 주소와 관리인 전화번호 정보 삽입
-        }
+        mHelper.close();
     }
 
 }
